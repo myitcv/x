57fbec98f218d1231729c81ea821f6e59302a3c6
Paul Jolly <paul@myitcv.io>
2018-05-18 17:57:52 +0100

    ci: precisely specify email notifications

--- _scripts/installGo.sh
+++ _scripts/installGo.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
-set -eu
+source "${BASH_SOURCE%/*}/common.bash"
 
 target=$HOME/gotip
 
-echo "Will install ${GOTIP_VERSION:0:10} to $target"
+echo "Will install ${GOTIP_VERSION:0:10} from $GOTIP_REPO to $target"
 
 mkdir -p $target
 cd $target
+
 if [ ! -e .git ]
 then
-	git clone -q https://github.com/golang/go .
+	echo cloning
+	git clone -q $GOTIP_REPO .
+else
+	echo fetching
+	git fetch -q $GOTIP_REPO
 fi
 
 git checkout -qf $GOTIP_VERSION
