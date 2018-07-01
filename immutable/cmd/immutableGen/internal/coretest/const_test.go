826cb6b276bc3a1e99a0d548491754a36590b315
Paul Jolly <paul@myitcv.io>
2018-07-01 18:17:52 +0100

    ci: use latest vgo and remove soon-to-be-updated packages

--- immutable/cmd/immutableGen/internal/coretest/const_test.go
+++
@@ -1,7 +0,0 @@
-package coretest_test
-
-const (
-	paul  = "paul"
-	peter = "peter"
-	age42 = 42
-)
--- vgo/go.mod
+++
@@ -1,3 +0,0 @@
-module myitcv.io/vgo
-
-require golang.org/x/net v0.0.0-20180406214816-61147c48b25b
