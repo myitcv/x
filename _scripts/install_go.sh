5e1eeca10e238733d31ddb6f4e1169a631da78b3
Paul Jolly <paul@myitcv.io>
2018-07-27 07:48:35 +0100

    ci: use new format of Go tip builds (#42)

--- _scripts/install_go.sh
+++ _scripts/install_go.sh
@@ -12,22 +12,20 @@ then
 	exit 1
 fi
 
+tf=$(mktemp)
 os=$(uname | tr '[:upper:]' '[:lower:]')
 arch="amd64"
 
 if [[ "$GO_VERSION" = go* ]]
 then
-	cd $HOME
-	curl -sL  https://dl.google.com/go/$GO_VERSION.${os}-${arch}.tar.gz | tar -zx
+	source="https://dl.google.com/go/$GO_VERSION.${os}-${arch}.tar.gz"
+	curl -sL $source > $tf
 else
-	# tip
-	source="https://s3-eu-west-1.amazonaws.com/io.myitcv.gobuilds/${os}_${arch}/$GO_VERSION.tar.gz"
-	target=$HOME/go
-
-	echo "Will install ${GO_VERSION:0:10} from $source to $target"
-
-	mkdir -p $target
-	cd $target
-
-	curl -L -s $source | tar -xz
+	source="s3://io.myitcv.gobuilds/${os}_${arch}/$GO_VERSION.tar.gz"
+	aws s3 cp $source $tf
 fi
+
+echo "Will install ${GO_VERSION} from $source"
+
+cd $HOME
+tar -zxf $tf
