1c12a089db6a2380fd66a2ef6a3707c12f594859
Paul Jolly <paul@myitcv.io>
2018-09-07 21:14:19 +0100

    react: upgrade bundled version to 16.5.0 (along with webpack) (#66)

--- react/internal/testutils/webpack.config.js
+++ react/internal/testutils/webpack.config.js
@@ -1,8 +1,10 @@
 const webpack = require("webpack");
 
 module.exports = {
+	mode: 'development',
   entry: "./entry.point",
   output: {
+		path: __dirname,
     filename: "testutils.inc.js",
     libraryTarget: "this",
   }
