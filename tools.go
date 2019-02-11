f5dff8a293a7376f9ba0712c2d3fb09f14b91ae2
Paul Jolly <paul@myitcv.io>
2019-02-11 10:44:38 +0000

    ci: upgrade vbash and tweak scripts to take advantage

--- tools.go
+++ tools.go
@@ -6,6 +6,7 @@ import (
 	_ "github.com/gopherjs/gopherjs"
 	_ "github.com/jteeuwen/go-bindata/go-bindata"
 	_ "github.com/myitcv/gobin"
+	_ "github.com/myitcv/vbash"
 	_ "golang.org/x/tools/cmd/goimports"
 	_ "golang.org/x/tools/cmd/stringer"
 )
